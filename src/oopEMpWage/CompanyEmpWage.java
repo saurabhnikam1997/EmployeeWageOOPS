50fbb5505c32c6d08a68818e8330e4294a746a0c
saurabhnikam1997 <nikam280197@gmail.com>
2020-11-01 08:46:28 +0530

    Emp Wage calculation using oop concepts

---
+++ src/oopEMpWage/CompanyEmpWage.java
@@ -0,0 +1,33 @@
+package oopEMpWage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+class CompanyEmpWage {
+	//attributes
+	  public final String company;
+	  public final int empRatePerHr;
+	  public final int numberOfWorkingDays;
+	  public final int maxHrPerMonth;
+	  public int totalEmpWage;
+	  ArrayList<Integer> dailyWage = new ArrayList<Integer>();
+
+	  //constructor
+	  public CompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth) {
+	    this.company = company;
+	    this.empRatePerHr = empRatePerHr;
+	    this.numberOfWorkingDays = numberOfWorkingDays;
+	    this.maxHrPerMonth = maxHrPerMonth;
+	  }
+
+	  //setter
+	  public void setTotalEmpWage(int totalEmpWage) {
+	    this.totalEmpWage = totalEmpWage;
+	  }
+	  //getter
+	  public String toString() {
+	    return "Total employee wages for: " + company + " is " + totalEmpWage;
+	  }
+
+}
---
+++ src/oopEMpWage/InEmployeewageoops.java
@@ -0,0 +1,6 @@
+package oopEMpWage;
+
+public interface InEmployeewageoops {
+	public void addCompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth);
+
+}
